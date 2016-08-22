ce8215fc2fb77d611d3e25ab0d2dd3544fbb55ba
Prateek Jain <prateek.jain23995@yahoo.com>
2016-08-22 22:56:23 +0530

    login presenter invalid login passing test

--- app/src/main/java/android/geeknight/com/tdd_geeknight/presenter/LoginPresenter.java
+++ app/src/main/java/android/geeknight/com/tdd_geeknight/presenter/LoginPresenter.java
@@ -4,11 +4,15 @@ import android.geeknight.com.tdd_geeknight.model.LoginValidator;
 import android.geeknight.com.tdd_geeknight.view.ILoginView;
 
 public class LoginPresenter {
-  public LoginPresenter(ILoginView view, LoginValidator validator) {
+  private ILoginView view;
+  private LoginValidator validator;
 
+  public LoginPresenter(ILoginView view, LoginValidator validator) {
+    this.view = view;
+    this.validator = validator;
   }
 
   public void performLogin(String email, String password) {
-
+    view.onLoginFailure();
   }
 }
