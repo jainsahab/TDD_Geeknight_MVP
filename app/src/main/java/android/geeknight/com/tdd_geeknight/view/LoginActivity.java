5c5df844a390019e3aee2b32b2eda8b3a96c670e
Prateek Jain <prateek.jain23995@yahoo.com>
2016-08-22 23:23:58 +0530

    login activity passing test

--- app/src/main/java/android/geeknight/com/tdd_geeknight/view/LoginActivity.java
+++ app/src/main/java/android/geeknight/com/tdd_geeknight/view/LoginActivity.java
@@ -1,15 +1,20 @@
 package android.geeknight.com.tdd_geeknight.view;
 
 import android.geeknight.com.tdd_geeknight.R;
+import android.geeknight.com.tdd_geeknight.model.LoginValidator;
+import android.geeknight.com.tdd_geeknight.presenter.LoginPresenter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, ILoginView {
 
   private EditText emailField;
   private EditText passwordField;
+  private LoginPresenter loginPresenter;
+  private TextView loginStatus;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +22,27 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     setContentView(R.layout.activity_login);
     emailField = (EditText) findViewById(R.id.email_text);
     passwordField = (EditText) findViewById(R.id.password_text);
+    loginStatus = (TextView) findViewById(R.id.login_status);
     findViewById(R.id.login_button).setOnClickListener(this);
+    loginPresenter = new LoginPresenter(this, new LoginValidator());
   }
 
   @Override
   public void onClick(View view) {
+    if(view.getId() == R.id.login_button) {
+      String email = emailField.getText().toString();
+      String password = passwordField.getText().toString();
+      loginPresenter.performLogin(email, password);
+    }
   }
 
   @Override
   public void onLoginSuccess() {
-
+    loginStatus.setText("Login Succeed");
   }
 
   @Override
   public void onLoginFailure() {
-
+    loginStatus.setText("Login Failure");
   }
 }
